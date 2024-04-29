5aea107009bcfb7e9ed35a5646cd6180a7d828ad
jckhoe <ongjackhoe@gmail.com>
2024-04-29 16:08:14 +0900

    Fix error handling

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func main() {
 	log.Println("Application has started")
 
 	if len(os.Args) < 2 {
-		log.Println("Invalid usage of application")
+		log.Fatalln("Invalid usage of application")
 	}
 	log.Printf("You have entered:  %s \n", os.Args[1])
 
